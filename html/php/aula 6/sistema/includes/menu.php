8a2429c74e65e982f17dabd33bf81ad5d786aafe
AlexRgds <133722844+AlexRgds@users.noreply.github.com>
2023-11-10 21:51:34 -0300

    Add files via upload

---
+++ html/php/aula 6/sistema/includes/menu.php
@@ -0,0 +1,17 @@
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="../View/index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="../View/cliente.php">Clientes</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="../View/fornecedor.php">Fornecedor</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="../View/produto.php">Produto</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Sair</a>
+  </li>
+</ul>
